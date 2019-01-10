3d54b1f9d1aa044618e274d2e0c13aa6ccce0f5c
pranav mishra <pranavmishra559@gmail.com>
2019-01-10 21:04:57 +0530

    Create return_subset_of_array.java

---
+++ assignment on advance recursion/return_subset_of_array.java
@@ -0,0 +1,58 @@
+
+import java.util.*;
+
+/**
+ * returnsubsetofarray
+ */
+public class returnsubsetofarray {
+
+    public static void main(String[] args) {
+
+        int[] arr = { 1, 2, 3 };
+        int[][] a = subsets(arr);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.println(a[i][j]);
+            }
+        }
+}
+public class solution {
+
+	// Return a 2D array that contains all the subsets
+	public static int[][] subsets(int input[]) {
+		// Write your code here
+      return subsets(input, 0);
+        
+    }
+    
+	   public static int[][] subsets(int input[], int start) {
+       if (start == input.length) {
+            int[][] myans = new int[1][0];
+            return myans;
+        }
+        int cn = input[start];
+        int[][] smallanswer = subsets(input, start + 1);
+        int[][] myans = new int[2 * smallanswer.length][];
+        int k = 0;
+        
+        for (int i = 0; i < smallanswer.length; i++) {
+            myans[k] = new int[smallanswer[i].length];
+            for (int j = 0; j < smallanswer[i].length; j++) {
+                myans[k][j] = smallanswer[i][j];
+
+            }
+            k++;
+        }
+        for (int i = 0; i < smallanswer.length; i++) {
+            myans[k] = new int[smallanswer[i].length + 1];
+            myans[k][0]=cn;
+            for (int j = 1; j <= smallanswer[i].length; j++) {
+                myans[k][j] = smallanswer[i][j - 1];
+            }
+            k++;
+        }
+        
+        return myans;
+
+	}
+}
