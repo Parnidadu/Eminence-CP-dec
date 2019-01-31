da1fbfe08845f0e1755d91ce0cae80221bde80b3
pranav mishra <pranavmishra559@gmail.com>
2019-01-31 23:18:21 +0530

    Create inversion_count.cpp

---
+++ backtracking/inversion_count.cpp
@@ -0,0 +1,58 @@
+long long merge(int A[],int left,int mid,int right){
+
+	int i=left,j=mid,k=0;
+
+	int temp[right-left+1];
+	long long count = 0;
+	while(i<mid && j<=right){
+		if(A[i] <= A[j]){
+			temp[k++] = A[i++];
+		}else{
+			temp[k++] = A[j++];
+			count += mid - i;
+		}
+	}
+	while(i<mid){
+		temp[k++] = A[i++];
+	}
+	while(j<=right){
+		temp[k++] = A[j++];
+	}
+
+	for(int i=left,k=0;i<=right;i++,k++){
+		A[i] = temp[k];
+	}
+	return count;
+}
+long long merge_sort(int A[],int left,int right){
+
+	long long count = 0;
+	if(right > left){
+		int mid = (left + right)/2;
+
+		long long countLeft = merge_sort(A,left,mid);
+		long long countRight = merge_sort(A,mid+1,right);
+		long long myCount = merge(A,left,mid+1,right);
+
+		return myCount + countLeft + countRight;
+	}
+	return count;
+
+}
+long long solve(int A[], int n)
+{
+	long long ans = merge_sort(A,0,n-1);
+	return ans;
+}
+#include<bits/stdc++.h>
+using namespace std;
+#define MAXN 100005
+int main()
+{
+	int n,A[MAXN];
+	cin>>n;
+	for(int i = 0; i < n ; i++){
+		cin>>A[i];
+	}
+	cout<<solve(A,n)<<endl;
+}
